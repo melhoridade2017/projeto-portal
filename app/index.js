8404732980473743520fad54de4be20e44cbb260
Eduardo Brito <eduardo.souza.brito@gmail.com>
2017-03-29 22:06:04 -0300

    Atualizacao da pagina de servicos

--- app/index.js
+++ app/index.js
@@ -4,5 +4,6 @@ var router  = express.Router();
 // Rota para o site
 router.get('/', require('./home/index'));
 router.get('/noticias', require('./noticias/index'));
+router.get('/servicos', require('./servicos/index'));
 
 module.exports = router;
---
+++ app/servicos/index.js
@@ -0,0 +1,5 @@
+var Service = function(req, res) {
+    res.render('servicos/index');
+};
+
+module.exports = Service;
