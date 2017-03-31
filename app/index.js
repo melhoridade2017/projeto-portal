b3bc05536df31f970cb0849ddbf584522c0df28e
Danilo Dias <dansandias@gmail.com>
2017-03-30 21:23:43 -0300

    Algum texto

--- app/index.js
+++ app/index.js
@@ -5,5 +5,6 @@ var router  = express.Router();
 router.get('/', require('./home/index'));
 router.get('/noticias', require('./noticias/index'));
 router.get('/fotos', require('./fotos/index'));
+router.get('/instalacoes', require('./instalacoes/index'));
 
 module.exports = router;
