d3bd54180e5ed4963998c83e2d116f0264586298
Rodrigo Costa <rodrigosilvadacosta@gmail.com>
2017-03-31 00:00:38 -0300

    Página Quem Somos

--- app/index.js
+++ app/index.js
@@ -4,5 +4,6 @@ var router  = express.Router();
 // Rota para o site
 router.get('/', require('./home/index'));
 router.get('/noticias', require('./noticias/index'));
+router.get('/quem-somos', require('./quem-somos/index'));
 
 module.exports = router;
